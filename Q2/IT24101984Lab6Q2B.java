5326f85504d0815c0dc5bcb45f211027bdb64a93
Vithanage T.A <it24101984@my.sliit.lk>
2024-08-29 19:35:36 +0530

    Add files via upload

---
+++ Q2/IT24101984Lab6Q2B.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class IT24101984Lab6Q2B  {
+public static void main(String[] args)  {
+
+ Scanner input = new Scanner(System.in);
+
+//Declare Variables
+ 
+ int num1,num2,num3,num4,num5,num6,num7,num8,num9,num10;
+
+//Get user inputs 
+ System.out.print("Enter a number : ");
+   num1 = input.nextInt();
+ 
+   System.out.print("Enter a number : ");
+   num2= input.nextInt();
+  
+  System.out.print("Enter a number : ");
+   num3= input.nextInt();
+
+  System.out.print("Enter a number : ");
+   num4= input.nextInt();
+
+ System.out.print("Enter a number : ");
+   num5= input.nextInt();
+
+
+ System.out.print("Enter a number : ");
+   num6= input.nextInt();
+
+System.out.print("Enter a number : ");
+   num7= input.nextInt();
+
+  System.out.print("Enter a number : ");
+   num8= input.nextInt();
+
+  System.out.print("Enter a number : ");
+   num9= input.nextInt();
+
+  System.out.print("Enter a number : ");
+   num10 = input.nextInt();
+
+
+//Print inpurt numbers
+  System.out.println("\n");
+  System.out.println("The numbers you enterd are : ");
+  System.out.print(num1 + " " + num2 + " " + num3 + " "+ num4 + " " + num5 + " " + num6 + " " + num7 + " " + num8 + " " + num9 + " " + num10);
+
+
+
+  }
+}
