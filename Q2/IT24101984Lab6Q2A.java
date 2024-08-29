35aa8f9ad7207752e3c6c07f6a56a8e43b3788d6
Vithanage T.A <it24101984@my.sliit.lk>
2024-08-29 19:35:12 +0530

    Add IT24101984Lab6Q2A , Q2B, Q2C .java to Q2

---
+++ Q2/IT24101984Lab6Q2A.java
@@ -0,0 +1,16 @@
+public class IT24101984Lab6Q2A  {
+public static void main(String[] args)  {
+
+ int num = 1;
+
+//loop
+ while (num <= 10)
+   {
+     System.out.print(num + " ");
+     num = num + 1;
+   }
+
+  
+
+ }
+} 
