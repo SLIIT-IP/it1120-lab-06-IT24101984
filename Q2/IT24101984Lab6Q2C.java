5e4044fdedffd46edeb827c5493acb5b45ca887f
Vithanage T.A <it24101984@my.sliit.lk>
2024-08-29 19:37:01 +0530

    Add IT24101984Lab6Q2C.java to Q2

---
+++ Q2/IT24101984Lab6Q2C.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class IT24101984Lab6Q2C  {
+public static void main(String[] args)  {
+
+ Scanner input = new Scanner(System.in);
+
+//Declare Variables
+ 
+ int num1,num2,num3,num4,num5,num6,num7,num8,num9,num10;
+ int sum;
+
+  
+ 
+ System.out.print("Enter a number : ");
+   num1 = input.nextInt();
+ 
+   System.out.print("Enter a number : ");
+   num2= input.nextInt();
+  
+  System.out.print("Enter a number : ");
+   num3= input.nextInt();
+
+  System.out.print("Enter a number : ");
+   num4= input.nextInt();
+
+ System.out.print("Enter a number : ");
+   num5= input.nextInt();
+
+
+ System.out.print("Enter a number : ");
+   num6= input.nextInt();
+
+System.out.print("Enter a number : ");
+   num7= input.nextInt();
+
+  System.out.print("Enter a number : ");
+   num8= input.nextInt();
+
+  System.out.print("Enter a number : ");
+   num9= input.nextInt();
+
+  System.out.print("Enter a number : ");
+   num10 = input.nextInt();
+
+
+
+//Calculate the sum
+sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+
+//Print inpurt numbers
+
+   System.out.println("\n");
+  System.out.println("The numbers you enterd are : ");
+  System.out.println(num1 + " " + num2 + " " + num3 + " "+ num4 + " " + num5 + " " + num6 + " " + num7 + " " + num8 + " " + num9 + " " + num10);
+
+  System.out.print("\n");
+
+  System.out.print("Sum of the nummbers : " + sum);
+
+
+
+  }
+}
