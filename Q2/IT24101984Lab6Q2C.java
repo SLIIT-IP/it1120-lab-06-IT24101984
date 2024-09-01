d251cd61195b254a0c8e96551d6539aa8a6fecdb
Vithanage T.A <it24101984@my.sliit.lk>
2024-09-01 20:20:31 +0530

    Add IT24101984Lab6Q2C.java to Q2

--- Q2/IT24101984Lab6Q2C.java
+++ Q2/IT24101984Lab6Q2C.java
@@ -8,6 +8,7 @@ public static void main(String[] args)  {
  
  int num1,num2,num3,num4,num5,num6,num7,num8,num9,num10;
  int sum;
+ double avg;
 
   
  
@@ -44,18 +45,22 @@ System.out.print("Enter a number : ");
 
 
 
-//Calculate the sum
+//Calculate the sum and average
 sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+ avg = sum / 10;
 
 //Print inpurt numbers
 
-   System.out.println("\n");
+  System.out.println("\n");
   System.out.println("The numbers you enterd are : ");
   System.out.println(num1 + " " + num2 + " " + num3 + " "+ num4 + " " + num5 + " " + num6 + " " + num7 + " " + num8 + " " + num9 + " " + num10);
 
   System.out.print("\n");
 
-  System.out.print("Sum of the nummbers : " + sum);
+  System.out.println("Sum of the nummbers : " + sum);
+
+  System.out.println("\n");
+  System.out.print("Average of the numbers : " + avg);
 
 
 
